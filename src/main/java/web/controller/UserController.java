3a450570156a6d7c1009542ea60298527b699356
Anatoliy Girak <agirak1@rambler.ru>
2022-09-04 16:43:26 +0300

    Исправление

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,56 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/users")
+    public String printUsers(ModelMap model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping(value = "/new")
+    public String newUser(ModelMap model) {
+        model.addAttribute("user", new User());
+        return "new";
+    }
+
+    @PostMapping(value = "/users")
+    public String saveNewUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(ModelMap model, @PathVariable("id") int id) {
+        model.addAttribute("user", userService.show(id));
+        return "edit";
+    }
+
+    @PostMapping("/users/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userService.update(id, user);
+        return "redirect:/users";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/users";
+    }
+}
